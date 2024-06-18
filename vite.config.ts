a2279863f3ce854bbc725cbe05095321ce8f9f28
Ludvig Hozman <ludvig123@gmail.com>
2024-06-18 14:07:45 +0200

    feat: Move to vite + vitest & upgrade deps

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+/// <reference types="vitest" />
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "NAVSpa",
+      fileName: "navspa",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  test: {
+    environment: "happy-dom",
+  },
+});
